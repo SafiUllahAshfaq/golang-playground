b3a27926238c96497b22b5c89f48c4c65e194d28
SafiUllahAshfaq <safiu799@gmail.com>
2022-06-19 20:43:13 +0500

    feat(map): create an indexed object

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -29,3 +29,20 @@ func randomFormat() string {
 
 	return formats[rand.Intn(len(formats))]
 }
+
+func Hellos(names []string) (map[string]string, error) {
+	// init empty array, syntax `make(map[key-type]value-type)`
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
--- hello.go
+++ hello.go
@@ -14,11 +14,18 @@ func main() {
 	fmt.Println("Bismillah")
 	fmt.Println(quote.Go())
 
-	message, err := greetings.Hello("Safi")
+	// message, err := greetings.Hello("Safi")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(message)
+
+	names := []string{"Safi", "Hassan", "Ali"}
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
